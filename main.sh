e63f3bc30d189b0a9852d89fa09ee7bc1b69b913
Aaron Ward <award7@wisc.edu>
2022-02-16 18:45:17 -0600

    simplified *.env aggregation

--- main.sh
+++ main.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
 # this is a wrapper to load shared (in the lab) but secret (to the world) environment variables from
-# our shared file server and overwrite the local .env variables, then do docker-compose up
+# our shared file server and overwrite the local .env.redcap variables, then do docker-compose up
 
 # get env variables to get location of airflow variables
 # https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-value-pairs
-export $(grep -v '^#' .env | xargs)
+shopt -s dotglob
+export $(grep -v '^#' .fs.env | xargs)
 
 # read shared airflow variables
-readarray -t keys < "${SHARED_AIRFLOW_KEYS_LOCATION}"
+shared=$(<"${SHARED_AIRFLOW_KEYS_LOCATION}")
 
-# parse the shared airflow variables into key-value pairs and then replace the .env variables appropriately
-# https://stackoverflow.com/questions/11245144/replace-whole-line-containing-a-string-using-sed
-for i in "${keys[@]}"; do
-  key=$( cut -d '=' -f 1 <<< "$i" );
-  value=$( cut -d '=' -f 2- <<< "$i" );
-  sed -i "s/.*${key}.*/${key}=${value}/" .env
-done
+# awk .env files into one for docker compose
+# https://stackoverflow.com/questions/8183191/concatenating-files-and-insert-new-line-in-between-files
+awk '{print $0}' *.env > .env
+
+echo "$shared" >> .env
 
 # start airflow environment
-docker compose up
+# docker compose up
+
+echo "finished"
